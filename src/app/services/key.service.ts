20257693c6537fb33ada804be0bd0afea318c268
chris <christian.hofer@hotmail.de>
2023-03-10 22:08:48 +0100

    update KeyManagementService to handle bech32 keys

--- src/app/services/key.service.ts
+++ src/app/services/key.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { generatePrivateKey, getPublicKey } from 'nostr-tools';
 import * as secp256k1 from '@noble/secp256k1'
+import { bech32 } from 'bech32';
+import { Buffer } from 'buffer';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +16,12 @@ export class KeyManagementService {
     return localStorage.getItem(this.PRIVATE_KEY) || '';
   }
 
-  setPrivKey(privkey: string): void {
-    localStorage.setItem(this.PRIVATE_KEY, privkey);
+  setPrivKey(privKey: string): void {
+    if(privKey.startsWith('nsec')) {
+      privKey = this.nsecToHex(privKey)
+    }
+
+    localStorage.setItem(this.PRIVATE_KEY, privKey);
   }
 
   removePrivKey(): void {
@@ -23,14 +29,33 @@ export class KeyManagementService {
   }
 
   isValidPrivateKey(privKey: string) {
+    if(privKey.startsWith('nsec')) {
+      privKey = this.nsecToHex(privKey)
+    }
+
     return secp256k1.utils.isValidPrivateKey(privKey)
   }
 
   generatePrivateKey(): string {
-    return generatePrivateKey(); // `sk` is a hex string
+    return this.hexToNsec(generatePrivateKey());
   }
 
   getPubKey(): string {
     return getPublicKey(this.getPrivKey());
+  }
+
+  hexToNsec(hexKey: string) {
+    const buf = Buffer.from(hexKey, 'hex');
+    const words = bech32.toWords(buf);
+    return bech32.encode('nsec', words);
+  }
+
+  nsecToHex(nsecKey: string) {
+    const { prefix, words } = bech32.decode(nsecKey);
+    if (prefix !== 'nsec') {
+      throw new Error('Invalid nsec-prefix format');
+    }
+    const buf = Buffer.from(bech32.fromWords(words));
+    return buf.toString('hex');
   }
 }
